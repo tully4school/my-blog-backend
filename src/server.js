fbb37fd93610dab62727945e8f9528762fcd329c
Joseph_Tully <tully4school@gmail.com>
2020-02-13 10:41:02 -0500

    added a post endpoint

--- src/server.js
+++ src/server.js
@@ -3,5 +3,6 @@ import express from "express";
 const app = express();
 
 app.get("/hello", (req, res) => res.send("Hello!"));
+app.post("/hello", (req, res) => res.send("Hello!"));
 
 app.listen(8000, () => console.log("Listening on port 8000"));
