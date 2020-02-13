0e8030c07796b427e78bba807ff240123d0fffee
Joseph_Tully <tully4school@gmail.com>
2020-02-13 11:14:37 -0500

    added a post request for the ability to add comments

--- src/server.js
+++ src/server.js
@@ -3,13 +3,16 @@ import bodyParser from "body-parser";
 
 const articlesInfo = {
 	"learn-react": {
-		upvotes: 0
+		upvotes: 0,
+		comments: []
 	},
 	"learn-node": {
-		upvotes: 0
+		upvotes: 0,
+		comments: []
 	},
 	"my-thoughts-on-resumes": {
-		upvotes: 0
+		upvotes: 0,
+		comments: []
 	}
 };
 
@@ -27,5 +30,12 @@ app.post("/api/articles/:name/upvote", (req, res) => {
 			`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes!`
 		);
 });
+app.post("/api/articles/:name/add-comment", (req, res) => {
+	const { username, text } = req.body;
+	const articleName = req.params.name;
 
+	articlesInfo[articleName].comments.push({ username, text });
+
+	res.status(200).send(articlesInfo[articleName]);
+});
 app.listen(8000, () => console.log("Listening on port 8000"));
