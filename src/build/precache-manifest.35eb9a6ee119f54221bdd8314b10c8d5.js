23b8c556c1f3bf54c9ed9af1d2b970703d413a36
Joseph_Tully <tully4school@gmail.com>
2020-02-14 14:57:12 -0500

    added a git ignore file setup for AWS

---
+++ src/build/precache-manifest.35eb9a6ee119f54221bdd8314b10c8d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee25115b3f8938cf371dae02e55b427a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f4c76b417a9e49e664da",
+    "url": "/static/css/main.bbd23df2.chunk.css"
+  },
+  {
+    "revision": "f31b8d3c58933b2727d5",
+    "url": "/static/js/2.4d99a4cd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.4d99a4cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4c76b417a9e49e664da",
+    "url": "/static/js/main.c7557922.chunk.js"
+  },
+  {
+    "revision": "1a6842cff40cdbd7ee88",
+    "url": "/static/js/runtime-main.d7f357ff.js"
+  }
+]);
